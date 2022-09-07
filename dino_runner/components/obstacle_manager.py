a0c1c3fc0d4617f75c2e4a1928a7595cbce57913
JosephHumerez <joseph_humerez@hotmail.com>
2022-09-07 13:22:29 -0400

    Guardado 02

--- dino_runner/components/obstacle_manager.py
+++ dino_runner/components/obstacle_manager.py
@@ -1,6 +1,9 @@
 from components.cactus import Cactus
 from utils.constants import LARGE_CACTUS, SMALL_CACTUS
 
+from components.bird import Bird
+from utils.constants import BIRD
+
 
 class ObstacleManager():
     def __init__(self):
@@ -10,6 +13,8 @@ class ObstacleManager():
         if len(self.obstacles) == 0:
             self.obstacles.append(Cactus(SMALL_CACTUS))
             self.obstacles.append(Cactus(LARGE_CACTUS))
+
+            self.obstacles.append(Bird(BIRD))
 
         for obstacle in self.obstacles:
             obstacle.update(15, self.obstacles)
